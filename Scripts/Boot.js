f8910351ff810fb73bf0b29d232d820863e730a5
Osybear <osybeargames@gmail.com>
2018-03-10 23:37:33 -0600

    Footer Game
    Website is under construction.
    Removed background extenders.

---
+++ Scripts/Boot.js
@@ -0,0 +1,18 @@
+var BasicGame = {};
+
+BasicGame.Boot = function (game) {
+};
+
+BasicGame.Boot.prototype = {
+
+    init: function () {
+    },
+
+    preload: function () {
+    },
+
+    create: function () {
+        this.state.start('Preloader');
+    }
+
+};
